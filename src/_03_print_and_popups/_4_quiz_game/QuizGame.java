f8305d1c8c102d357b3a9b3ed5cd462a84f50c96
root <root@5f8ae2057ace>
2020-06-19 18:53:12 +0000

    Last day of coding

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,16 +1,52 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
+		String question = JOptionPane.showInputDialog("Who was the first president of the United States?");
+		if(question.equals("George Washington")) {
+			JOptionPane.showMessageDialog(null, "Congrats! You are correct!");
+			score+=1;
+		
+		}else { 
+			JOptionPane.showMessageDialog(null,"WRONG ANSWER!");
+		}
+		String answer = JOptionPane.showInputDialog("What year is it?");
+		if(answer.equals("2020")) {
+			JOptionPane.showMessageDialog(null, "Congrats! You are correct!");
+			score+=1;
+		}else {
+			JOptionPane.showMessageDialog(null, "WRONG ANSWER!");
+		
+		}
+		String wonder = JOptionPane.showInputDialog("Who is the main charachter in Naruto?");
+		if(wonder.equals("Naruto")) {
+			JOptionPane.showMessageDialog(null, "Congrats! You are correct!");
+			score+=1;
+		}else { 
+			JOptionPane.showMessageDialog(null, "WRONG ANSWER!");
+		}
+		
+		// 3.  Use an if statement to check if their answer is correct
+			
+			
+			
+
+			
+		
+			
+			
+			
+			
+			
+			
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
