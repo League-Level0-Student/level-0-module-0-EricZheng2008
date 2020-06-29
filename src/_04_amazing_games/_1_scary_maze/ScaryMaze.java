8e4231ba2a4f3e79fb30aa41a327eb3f849150c3
root <root@26ff370ab752>
2020-06-29 18:30:29 +0000

    We learned more things. DAY ONE

--- src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
+++ src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
@@ -21,8 +21,8 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	
 	boolean started = false;
 	BufferedImage maze;
-	final int frameWidth = 500;
-	final int frameHeight = 500;
+	final int frameWidth = 1000;
+	final int frameHeight = 1200;
 
 	ScaryMaze() throws Exception {
 		
@@ -32,7 +32,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		
 		
 		//2. Change the line of code below so that it uses YOUR maze's file name
-		maze = ImageIO.read(getClass().getResource("standardMaze.png"));
+		maze = ImageIO.read(getClass().getResource("Maze.jpg.png"));
 		
 		
 		//3. Run the program. Do you see your maze? Don't continue until you do.
@@ -50,11 +50,12 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseColor = maze.getRGB(mouseX, mouseY);	
 		
 		//4. Print the mouseColor variable (Hint: use syso)
+		System.out.println( mouseColor);
 		
 		//5.  Run your program and move your mouse over the START COLOR. A number will be printed to the console
 		
 		
-		int startColor=0;
+		int startColor=-26624;
 		//6. Change the value of this startColor variable to the number printed in the previous step. 
 		
 		
@@ -66,6 +67,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		
 		
 		//7. Make a new int variable for the background color of the maze
+		int background = -7617718;
 
 		
 		//8. Run the program and move the mouse over the BACKGROUND COLOR. 
@@ -78,13 +80,20 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		
 			//9. If the mouse falls off the path (which means it is on the background color)
 			//		call the scare method - scare();
+			if (mouseColor == -7617718);{
+			JOptionPane.showMessageDialog(null, " Better Luck Next time. You lose");
+			}
 			
 			//13.  If the mouse is on the end color, pop up a message to tell them they won!
 			//    (you will need to get the number of the END COLOR by moving the mouse over it)
+		//	if (mouseColor == )
+			if (mouseColor == 16728876); {
+				JOptionPane.showMessageDialog(null, "YOU WIN!");
+			}
 
 					
 			
-		}	
+			}
 	}
 
 	private void scare() {
